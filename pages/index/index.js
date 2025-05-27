14fa7f5cfd4e51de20068f3a59901590e4a60a99
rscv5 <renshuchan@163.com>
2025-05-27 17:43:49 +0800

    首页优化v1.0,完成页面布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,15 +2,10 @@
 const app = getApp()
 const auth = require('../../utils/auth');
 
+
 Page({
   data: {
-    // 社区信息
-    communityInfo: {
-      name: '明发社区',
-      description:'明发滨江新城一期坐落于滨江大道1号，南京长江大桥北堡下...',
-      alldesc:'明发滨江新城一期坐落于滨江大道1号，南京长江大桥北堡下。小区交付于2006年，共有63栋楼房，建筑面积56万平方米，共3973户，划分为9个网格。2024年，社区不断探索小区和谐治理新模式，以“党建引领、多元共治、民主协商、科技赋能”为主线，建立“网格+物业”融合联动工作机制，明发一期物业在社区党委指导下，在融合服务党支部协助下，积极推进网格+物业全方位融合，实现网格员和物业管家联动互动融动，通过首问负责制、居民代表会商制等方式，努力做到小事不出楼栋，大事不出小区，难事不出社区。',
-      
-    },
+    
     showIntro: false, // 控制简介弹窗显示状态
     // 网格+物业联动结构图列表
     partyActivities: [
@@ -25,47 +20,161 @@ Page({
         title: '组织架构图',
       }
     ],
+
+    // 网格+物业联动工作风采
+    gridGallery:[
+      {
+        title:"安全走访",
+        images:[
+          { 
+            src:"https://img.picui.cn/free/2025/05/27/68356a6e63016.jpg",
+            caption:"网格员和物业管家一起检查弱电箱杂物"
+          },
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356b6b16584.jpg",
+            caption:"网管员和物业一起巡查小区路桩"
+          }
+        ],
+      },
+      {
+        title:"拆除违建",
+        images:[
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356c2e8dbb8.jpg",
+            caption:"拆除112栋一楼违建"
+          },
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356c87c013c.jpg",
+            caption:"拆除143楼顶违建"
+          }
+        ],
+      },
+      {
+        title:"关爱老人",
+        images:[
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68357f103e9fa.jpg",
+            caption:"网格员和物业管家走访独居老人"
+          },
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356dcfa741e.jpg",
+            caption:"网格员和物业管家走访独居老人"
+          }
+        ],
+      },
+      {
+        title:"小区整治",
+        images:[
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356e5c89502.jpg",
+            caption:"网格员和物业管家共同清理楼道杂物"
+          },
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356e5b5b2a2.jpg",
+            caption:"网格员和物业管家共同巡查小区"
+          }
+        ],
+      },
+      {
+        title:"议事协商",
+        images:[
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356eddb810e.jpg",
+            caption:"2024年10月10日一期房屋渗水维修施工单位选聘"
+          },
+          {
+            src:"https://img.picui.cn/free/2025/05/27/68356edc5f90e.jpg",
+            caption:"2025年4月14日一期管委会消防电系统会议"
+          }
+        ]
+      }
+    ],
+    
     // 今昔对比图列表
     compareList:[
       {
         id: 1,
-        before: '/assets/images/jingxi/105be.jpg',
-        after: '/assets/images/jingxi/105af.jpg',
-        title: '105号楼',
+        before: 'https://img.picui.cn/free/2025/05/27/683571b7c5509.jpg',
+        after: 'https://img.picui.cn/free/2025/05/27/683571ba345e4.jpg',
+        title: '105栋西边围墙整改',
       },
       {
         id: 2,
-        before: '/assets/images/jingxi/107be.jpg',
-        after: '/assets/images/jingxi/107af.jpg',
-        title: '107号楼',
+        before: 'https://img.picui.cn/free/2025/05/27/683572787ef95.jpg',
+        after: 'https://img.picui.cn/free/2025/05/27/6835727c5b8b3.jpg',
+        title: '110栋西边围墙',
       },  
       {
         id: 3,
-        before: 'https://img.picui.cn/free/2025/05/26/68347314aed96.jpg',
-        after: 'https://img.picui.cn/free/2025/05/26/68347314aed96.jpg',
-        title: '今昔对比图',
+        before: 'https://img.picui.cn/free/2025/05/27/68357308e3c7d.png',
+        after: 'https://img.picui.cn/free/2025/05/27/683572ca130a4.png',
+        title: '115栋侧面绿化及路面改造',
+      },
+      {
+        id:4,
+        before:'https://img.picui.cn/free/2025/05/27/68357354dc00c.png',
+        after:'https://img.picui.cn/free/2025/05/27/6835735754b69.png',
+        title:'128栋宅间道绿化和花坛改造',
+      },
+      {
+        id:5,
+        before:'https://img.picui.cn/free/2025/05/27/683573fe18f81.png',
+        after:'https://img.picui.cn/free/2025/05/27/683573d4966c4.jpg',
+        title:'135栋前停车位改造'
+      },
+      {
+        id:6,
+        before:'https://img.picui.cn/free/2025/05/27/68357496da0b0.png',
+        after:'https://img.picui.cn/free/2025/05/27/68357497ece17.png',
+        title:'138栋前休闲区改造'
+      },
+      {
+        id:7,
+        before:'https://img.picui.cn/free/2025/05/27/6835752d51842.png',
+        after:'https://img.picui.cn/free/2025/05/27/6835752f52a6c.jpg',
+        title:'161栋增加电动车棚'
+      },
+      {
+        id:8,
+        before:'https://img.picui.cn/free/2025/05/27/683575cb58056.png',
+        after:'https://img.picui.cn/free/2025/05/27/683575cdbdfae.jpg',
+        title:'网球场旁河道改造'
       }
     ],
     // 活动列表
     activities: [
       {
         id: 1,
-        cover: '/assets/images/huodong/hd3.jpg',
+        cover: 'https://img.picui.cn/free/2025/05/27/68357976848f3.jpg',
         title: '雷锋日活动',
         date:'2025-01-01',
-        location:'社区活动室'
+        location:'明发社区活动点'
       },
       {
         id: 2,
-        cover: '/assets/images/huodong/hd5.jpg',
+        cover: 'https://img.picui.cn/free/2025/05/27/6835794bf2994.jpg',
         title: '迎春文艺汇演活动',
         date:'2025-01-02',
         location:'社区广场'
       },
       {
         id: 3,
-        cover: '/assets/images/huodong/hd4.jpg',
+        cover: 'https://img.picui.cn/free/2025/05/27/683579c2bacda.jpg',
+        title: '义卖活动',
+        date:'2025-01-03',
+        location:'明发社区'
+      },
+      {
+        id: 4,
+        cover: 'https://img.picui.cn/free/2025/05/27/68357a33377cd.jpg',
         title: '写春联活动',
+        date:'2025-01-03',
+        location:'明发社区'
+      },
+      {
+        id:5,
+        conver: 'https://img.picui.cn/free/2025/05/27/68357a6e7658c.jpg',
+        title: '便民活动',
         date:'2025-01-03',
         location:'明发社区'
       }
@@ -74,25 +183,11 @@ Page({
     notices: [
       {
         id: 1,
-        cover: '/assets/images/banner1.png',
+        cover: 'none',
         title: '社区公示1',
         date:'2025-01-01',
         desc:'党建之星公示。'
       },
-      {
-        id: 2,
-        cover: '/assets/images/banner2.png',
-        title: '社区公示2',
-        date:'2025-01-02',
-        desc:'文明家庭公示。'
-      },
-      {
-        id: 3,
-        cover: '/assets/images/banner3.png',
-        title: '社区公示3',
-        date:'2025-01-03',
-        desc:'社区建设进展公示。'
-      }
     ],
 
   },
@@ -119,12 +214,7 @@ Page({
     })
   },
 
-  // 导航到网格+物业联动页面
-  onMoreParty() {
-    wx.navigateTo({
-      url: '/pages/index/wgwyld'
-    })
-  },
+
 
   // 网格+物业图片点击
   onPartyTap(e) {
@@ -137,6 +227,16 @@ Page({
         url: `/pages/index/wgwyld?img=${encodeURIComponent(item.cover)}&title=${encodeURIComponent(item.title)}`
       });
     }
+  },
+
+
+  // 预览图片方法
+  previewImage(e) {
+    const src = e.currentTarget.dataset.src;
+    wx.previewImage({
+      current: src,
+      urls:[src]
+    });
   },
 
   //跳转到今昔对比网页
--- pages/index/wgwyld.js
+++ pages/index/wgwyld.js
@@ -28,5 +28,13 @@ Page({
         title: '图片加载失败',
         icon: 'none'
       });
+    },
+
+    // 预览图片方法
+    previewImage() {
+      wx.previewImage({
+        current: this.data.img, // 当前显示图片的链接
+        urls: [this.data.img]   // 需要预览的图片链接列表
+      });
     }
-  });
+});
